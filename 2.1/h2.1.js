fba23269b144aef62c8b1b1d6c50ce65da5542c2
Mehran <mshahbackhsh@roc-dev.com>
2020-01-20 09:07:21 +0100

    Initial commit

---
+++ 2.1/h2.1.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById("Paginatitel").innerText= "Dit is de paginatitel";
+document.getElementById("Subkop").innerText= "Dit is de subkop";
+document.getElementById("trash1").src= "trash1.jpg";
+document.getElementById("trash2").src="trash2.jpg";
---
+++ h2.2/h2.2.js
@@ -0,0 +1,4 @@
+document.getElementById("blauw").style.background="#00ffff";
+document.getElementById("groen").style.background="#009900";
+document.getElementById("geel").style.background="#ffff00";
+document.getElementById("rood").style.background="#ff0000";
